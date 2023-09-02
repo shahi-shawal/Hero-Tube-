c09314abcacd3835414a6588194f990415cdb8cd
Shawal <shahishawal@gmail.com>
2023-09-02 23:58:39 +0600

    add sort

--- tube.js
+++ tube.js
@@ -2,7 +2,6 @@ const loadCategory = async()=>{
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
     const getData= data.data;
-    // console.log(getData);
     displayCategory(getData);
 }
 
@@ -21,15 +20,10 @@ const displayCategory = (getData)=>{
       const res= await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
       const data = await res.json();
       const getData2 = data.data;
-    //   console.log(getData2);
       contentDisplay(getData2);
-
-      
  } 
-
-
+ 
  const contentDisplay = (getData2)=>{
-     
     const contetCard = document.getElementById('contetCard');
     contetCard.textContent="";
     if (getData2.length > 0) {
@@ -83,82 +77,92 @@ const displayCategory = (getData)=>{
  }
 
 
-// var sortFun=(getData2)=>{
-    
-//  }
+ const displaySort = async(id)=>{
+    const res = await fetch`https://openapi.programming-hero.com/api/videos/category/1000`
+    const data = await res.json();
+    const data2= data.data
+    sortFun(data2)
+    const contetCard = document.getElementById('contetCard');
+    contetCard.textContent="";
+    if (data2.length > 0) {
+        
+        data2?.forEach((content) => {
+           
 
-const displaySortData =()=>{
-         const contentBtn = async(id)=>{
-            const res= await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
-            const data = await res.json();
-            const getData2 = data.data;
-
-
-            getData2.sort((a,b)=>{
-                let x = parseInt(a.others.views);
-                let y = parseInt(b.others.views);
-                if (x > y) {
-                    return -1
-                }
-                else if (x < y) {
-                    return 1
-                }
-                else{
-                    return 0
-                }
-            })
-        // sortFun(getData2)
+            const div = document.createElement('div');
+            div.className = `card bg-base-100 shadow-xl mt-6`;
+            div.innerHTML=`
+                    <figure class="w-70 h-80">
+                    <div class="indicator">
+                    <span class="indicator-item indicator-bottom indicator-end mx-[110px] bg-black text-white">${content.others.posted_date? ((Math.floor(content.others.posted_date/3600))+"hr ")+ (Math.floor(content.others.posted_date%3600/60)+"min ") +
+                    (Math.floor(content.others.posted_date%60)+" sec ago") : ""
+                }</span> 
+                    <img class="w-70 h-60" src="${content?.thumbnail}  " alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                    <div class="avatar">
+                    <div class="w-14 rounded-full">
+                        <img src="${content.authors[0].profile_picture}" />
+                    </div>
+                    <h2 class="card-title p-2">${content.title}</h2>
+                    </div>
+                    <div class="mx-16">
+                    <p class="">${content.authors[0].profile_name}${content.authors[0].verified? '<img src="./bluetik.png">' : ""  }</p>
+                    <p>${content.others.views} views </p>
+                    </div>
             
-            getData2.forEach((sortedData)=>{
-                
+            `
+            contetCard.appendChild(div)
+
+        }
+    )  
+    } else {
     
-                const div = document.createElement('div');
-                div.className = `card bg-base-100 shadow-xl mt-6`;
-                div.innerHTML=`
-                        <figure class="w-70 h-80">
-                        <div class="indicator">
-                        <span class="indicator-item indicator-bottom indicator-end mx-[110px] bg-black text-white">${sortedData.others.posted_date? ((Math.floor(sortedData.others.posted_date/3600))+"hr ")+ (Math.floor(sortedData.others.posted_date%3600/60)+"min ") +
-                        (Math.floor(sortedData.others.posted_date%60)+" sec ago") : ""
-                    }</span> 
-                        <img class="w-70 h-60" src="${sortedData?.thumbnail}  " alt="Shoes" />
-                        </figure>
-                        <div class="card-body">
-                        <div class="avatar">
-                        <div class="w-14 rounded-full">
-                            <img src="${sortedData.authors[0].profile_picture}" />
-                        </div>
-                        <h2 class="card-title p-2">${sortedData.title}</h2>
-                        </div>
-                        <div class="mx-16">
-                        <p class="">${sortedData.authors[0].profile_name}${sortedData.authors[0].verified? '<img src="./bluetik.png">' : ""  }</p>
-                        <p>${sortedData.others.views} views </p>
-                        </div>
+        const div = document.createElement('div');
+        div.className = `` 
+        div.innerHTML= `
+                     
+                       <div class="flex justify-center items-center">
+                       <div class="mt-20 pr-96 mr-[-1500px]">
+                       <img class= "w-auto h-80" src="./icon.png">
+                       <h2 class="text-4xl font-bold">Oppps!! No Data Found</h2>
+                       </div>
+                       </div>
                 
-                `
-                contetCard.appendChild(div)
-                
-            })
-      
-       }
-     
-   
+        `
+        contetCard.appendChild(div);
+}
+ }
+
+
+
+const sortFun = (data)=>{
+    data.sort((a,b)=>{
+        let x = parseInt(a.others.views);
+        let y = parseInt(b.others.views);
+        if (x > y) {
+            return -1
+        }
+        else if (x < y){
+            return 1
+        }
+        else{
+            return 0
+        }
+    })
 }
 
 
-//  document.getElementById('sortId').addEventListener('click',function () {
-    // console.log(sortFun)
+
+const sortBtn=()=>{
+    displaySort();
+}
 
 
- 
 
-   
-      
-        const sortBtn=()=>{
-       displaySortData(true)
-        }
-//  })
- 
- 
+
+
+
 
 
 
